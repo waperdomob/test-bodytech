9949c78f7e7965b6062262562a80028c45ac9a57
Wilmer Perdomo <wilmerpb30@gmail.com>
2024-05-22 10:36:47 -0500

    remove vite, don't work

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,8 +9,6 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCb1yis3T2mUXbvyvAMr1I9+U46RDW5wUqUKlroj+T+9F8g4t6t0eO+G2qNpiY" crossorigin="anonymous">
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    @vite(['resources/sass/app.scss', 'resources/js/bootstrap.js'])
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
